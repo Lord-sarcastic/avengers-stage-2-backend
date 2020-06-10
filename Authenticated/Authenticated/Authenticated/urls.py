1da37b696ff1d749c885424003d8381a4858e535
matt-marly <mattadeyemi15r@gmail.com>
2020-06-10 14:56:17 +0100

    Resolved merge conflict by incorporating both suggestions.

--- Authenticated/Authenticated/Authenticated/urls.py
+++ Authenticated/Authenticated/Authenticated/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 
+
 from django.urls import path,include
 
 urlpatterns = [
@@ -36,4 +37,21 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration', include('dj_rest_auth.registration.urls'))
 >>>>>>> Aeesha:Feature: Included urls for registration and provided a doc.http with url calls to test that it is working fine
+
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'rest-auth/', include('dj_rest_auth.urls'))
+>>>>>>> Reverted commit
 ]
+
+if getattr(settings, 'REST_USE_JWT', False):
+    from rest_framework_simplejwt.views import (
+        TokenRefreshView, TokenVerifyView,
+    )
+
+    urlpatterns += [
+        path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ]
